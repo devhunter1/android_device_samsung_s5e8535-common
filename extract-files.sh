dcc4d434fa842bd3dc8d89982d9cfeed0c579223
Tim Zimmermann <tim@linux4.de>
2025-02-05 13:57:23 +0530

    s5e8535-common: replace with protobuf 3.9.1-vendorcompat
    * Required by RIL and widevine DRM
    * Patch widevine to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,9 @@ function blob_fixup() {
         vendor/lib64/libsec-ril.so)
             sed -i 's/ril.dds.call.ongoing/vendor.calls.slot_id/g' "${2}"
             ;;
+        vendor/lib64/libwvaidl.so)
+            "${PATCHELF}" --replace-needed libprotobuf-cpp-lite-3.9.1.so libprotobuf-cpp-full-3.9.1.so "${2}"
+            ;;
     esac
 }
 
