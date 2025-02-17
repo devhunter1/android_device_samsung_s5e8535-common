75e79753079bc28434f8d28b243ad3dda9cf4c01
devhunter1 <anubhabkundu2@gmail.com>
2025-02-17 13:53:09 +0530

    universal8535-common: include lineage libperfmgr sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,6 +182,7 @@ VENDOR_SECURITY_PATCH := 2024-12-01
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
 include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
 
 # Vendor_boot
