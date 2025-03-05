83c4d329ec9130aab297e578e1dc7012ae87190d
devhunter1 <anubhabkundu2@gmail.com>
2025-03-05 12:39:29 +0530

    s5e8535-common: replace libcppbor.so with libcppbor_external.so
    * Requires after Android V
    
    Signed-off-by: devhunter1 <anubhabkundu2@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,7 @@ function blob_fixup() {
         vendor/bin/hw/android.hardware.security.keymint-service|vendor/lib*/libskeymint*.so)
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             "${PATCHELF}" --replace-needed libcrypto.so libcrypto-v33.so "${2}"
+            "${PATCHELF}" --replace-needed libcppbor_external.so libcppbor.so "${2}"
             ;;
     esac
 }
