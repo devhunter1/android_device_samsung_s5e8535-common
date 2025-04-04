fc111787570c476779a351b2c38dbebe7f14587e
devhunter1 <anubhabkundu2@gmail.com>
2025-04-04 12:33:00 +0530

    s5e8535-common: build lineage touch HAL
    Change-Id: I277c071f8a9b238c96349a1151fce7d3d34ed156

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,7 +160,8 @@ TARGET_USES_MKE2FS := true
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/configs/vintf/device_framework_matrix.xml
+    $(COMMON_PATH)/configs/vintf/device_framework_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
