5a8f7fc42cbcb14a81adb2337ab90b9fe7383dce
devhunter1 <anubhabkundu2@gmail.com>
2025-04-12 12:33:00 +0530

    s5e8535-common: align PRODUCT_AAPT_PREF_CONFIG to stock values
    Change-Id: Iaa9ff3602f45fccc9e5c4d5c1556cd2be2a3298e

--- common.mk
+++ common.mk
@@ -104,7 +104,7 @@ PRODUCT_COPY_FILES += \
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 420dpi
+PRODUCT_AAPT_PREF_CONFIG := 450dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # FastCharge
