ed08d1268d1fe5664b6b3ee67289e42ea3ffc0ae
devhunter1 <anubhabkundu2@gmail.com>
2025-08-08 12:05:53 +0530

    s5e8535-common: build vib hal from source
    Change-Id: I981392f9b884e380e91f6f511b5f09db62f38ba8

--- common.mk
+++ common.mk
@@ -297,6 +297,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.samsung
+
 # WIFI
 PRODUCT_PACKAGES += \
     hostapd \
