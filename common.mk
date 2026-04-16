870a0abd40e7768f9f77b681ae4c16b60364f59e
devhunter1 <anubhabkundu2@gmail.com>
2026-04-16 22:59:42 +0530

    s5e8535: Initial tree

---
+++ common.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/s5e8535-common
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := phone
