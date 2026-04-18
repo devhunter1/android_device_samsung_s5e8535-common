6c7eb072c31200d21467055e959cde932b6c4653
devhunter1 <anubhabkundu2@gmail.com>
2026-04-18 12:59:46 +0530

    s5e8535-common: Add Initial proprietary blobs extraction support
    Change-Id: I736a89ce473946f8ef236fe57351e445fc493ad8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,6 +8,9 @@ COMMON_PATH := device/samsung/s5e8535-common
 
 BUILD_BROKEN_DUP_RULES := true
 
+# Inherit the proprietary files
+include vendor/samsung/s5e8535-common/BoardConfigVendor.mk
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
@@ -129,6 +132,9 @@ BOARD_RECOVERY_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board $(B
 BOARD_RECOVERY_MKBOOTIMG_ARGS += --dtb_offset 0x00000000
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2025-12-01
 
 # Userdata
 TARGET_USERIMAGES_USE_F2FS := true
