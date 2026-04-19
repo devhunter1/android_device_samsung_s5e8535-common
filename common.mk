0eb8bc8039b22b0c9b3c9585ac80993aef28ac4c
Tim Zimmermann <tim@linux4.de>
2026-04-19 13:48:09 +0530

    s5e8535-common: Introduce sehradiomanager
    Change-Id: Ic24a9e4cc2b509f04a473009a9b10fba2b984a9b

--- common.mk
+++ common.mk
@@ -115,6 +115,13 @@ PRODUCT_PACKAGES += \
 # Product characteristics
 PRODUCT_CHARACTERISTICS := phone
 
+# RIL
+PRODUCT_PACKAGES += \
+    sehradiomanager
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ril/sehradiomanager.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sehradiomanager.conf
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.samsung-multihal
