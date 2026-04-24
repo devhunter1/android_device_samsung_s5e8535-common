fe338632d4cb520c7d7fe05ccc4ab25ebad4f05b
devhunter1 <anubhabkundu2@gmail.com>
2026-04-24 19:42:48 +0530

    s5e8535-common: Include missing permission android.hardware.telephony.satellite.xml
    Change-Id: I5b37d98c76e2a3949bed7ec23035975cb416440d

--- common.mk
+++ common.mk
@@ -255,7 +255,8 @@ $(call inherit-product, hardware/samsung_slsi-linaro/config/config.mk)
 # Telephony
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.hardware.telephony.satellite.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.satellite.xml
 
 # Thermal
 PRODUCT_PACKAGES += \
